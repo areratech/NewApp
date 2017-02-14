53850bb6f0d9e85574311ce838adaa2f15649edf
Andi <areraareratech@gmail.com>
2017-02-13 19:08:32 -0800

    update button

--- app/src/main/java/excellential_client/android/com/newapp/MainActivity.java
+++ app/src/main/java/excellential_client/android/com/newapp/MainActivity.java
@@ -2,12 +2,16 @@ package excellential_client.android.com.newapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button pullcode;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pullcode = (Button) findViewById(R.id.pullcode);
+
     }
 }
